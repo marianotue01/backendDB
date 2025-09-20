165dcab982e6196c00a130d60dbe1f915793ef60
Mariano Tuero <mariano.tuero@gmail.com>
2025-09-20 17:10:10 +0200

    swagger

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ import userProductRoutes from "./routes/userProducts.js";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
