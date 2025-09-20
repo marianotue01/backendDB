6c01c6559d123d13c27f3680a7c144ccbd5049e2
Mariano Tuero <mariano.tuero@gmail.com>
2025-09-20 18:11:56 +0200

    test

--- api/server.js
+++ api/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB
+// MongoDB V
 mongoose
   .connect(process.env.MONGO_URI, { dbName: "store" })
   .then(() => console.log("✅ MongoDB connected"))
