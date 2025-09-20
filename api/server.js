9a547aa20f1d50ae54fd97a8500c74fe999aa68b
Mariano Tuero <mariano.tuero@gmail.com>
2025-09-20 16:48:03 +0200

    swagger

--- api/server.js
+++ api/server.js
@@ -16,19 +16,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
+// MongoDB
 mongoose
   .connect(process.env.MONGO_URI, { dbName: "store" })
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-// API routes
+// Rutas
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/user-products", userProductRoutes);
 
-// Swagger
+// Swagger UI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Start server
 app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
