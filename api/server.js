e5b93b060856a47927cb906126d08e459eee1b48
Mariano Tuero <mariano.tuero@gmail.com>
2025-09-20 18:15:31 +0200

    redirigiendo al swagger como default

--- api/server.js
+++ api/server.js
@@ -16,13 +16,13 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB V
+// Conexión a MongoDB
 mongoose
   .connect(process.env.MONGO_URI, { dbName: "store" })
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-// Rutas
+// Rutas API
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/user-products", userProductRoutes);
@@ -30,4 +30,10 @@ app.use("/api/user-products", userProductRoutes);
 // Swagger UI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// Redirigir la raíz al Swagger UI
+app.get("/", (req, res) => {
+  res.redirect("/api-docs");
+});
+
+// Iniciar servidor
 app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
